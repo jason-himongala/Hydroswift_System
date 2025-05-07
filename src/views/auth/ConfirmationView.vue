6842b6e44d9b3c2abaf1897c7291b684967a809f
RCempron <ronaldcempronasis@gmail.com>
2025-05-07 11:08:09 +0800

    fix: fix registration message

--- src/views/auth/ConfirmationView.vue
+++ src/views/auth/ConfirmationView.vue
@@ -10,7 +10,7 @@
           <v-icon size="64" color="green">mdi-check-circle</v-icon>
           <h2 class="mt-4" style="color: #0d47a1">Registration Successful!</h2>
           <p class="mt-2" style="color: #1976d2">
-            Thank you for registering your water station with us.
+            Thank you for registering with us. We are excited to serve you!
           </p>
           <v-btn class="mt-4" color="blue-darken-2" @click="$router.push('/Homepage')">
             Go to Home</v-btn
